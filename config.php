af61382b0a4c6ce11d7e8d5e5b240abd7d46242c
Thomas Rumpf <72502304+thomas-rumpf@users.noreply.github.com>
2020-12-29 12:20:46 +0100

    Create config.php

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+$CONFIG = array (
+  'memcache.local' => '\\OC\\Memcache\\APCu',
+  'memcache.distributed' => '\\OC\\Memcache\\Redis',
+  'memcache.locking' => '\\OC\\Memcache\\Redis',
+  'redis' => [
+    'host' => 'xxx.xxx.xxx.xxx', # set your host IP address here
+    'port' => 6379,
+  ],
+  'datadirectory' => '/data',
+  'instanceid' =>  # will be created automatically during container initialization 
+  'passwordsalt' =>  # will be created automatically during container initialization
+  'secret' =>  # will be created automatically during container initialization
+  'trusted_domains' => 
+  array (
+    0 => 'xxx.xxx.xxx.xxx', # set your host IP address here
+  ),
+  'dbtype' => 'mysql',
+  'version' => '20.0.3.2',
+  'overwrite.cli.url' => 'https://xxx.xxx.xxx.xxx', # set your host IP address here
+  'dbname' => 'ncdb',
+  'dbhost' => 'db',
+  'dbport' => '',
+  'dbtableprefix' => 'oc_',
+  'dbuser' => 'nextcloud',
+  'dbpassword' => 'xxxx',
+  'installed' => true,
+);
